b55a32e6a5a5e7d419a7f7d504e79a5a7ccc5c6d
samuelcolvin <s@muelcolvin.com>
2014-04-01 23:44:23 +0100

    removed pull

--- urls.py
+++ urls.py
@@ -1,10 +1,12 @@
 from django.conf.urls import patterns, url, include
-<<<<<<< HEAD
-import SkeletalDisplay.editor as editor
-import SkeletalDisplay.views as views
-import SkeletalDisplay.views_base as viewb
+import HotDisplay.editor as editor
+import HotDisplay.views as views
+import HotDisplay.views_base as viewb
 
-urlpatterns = patterns('SkeletalDisplay.views',
+urlpatterns = patterns('HotDisplay.views',
+#    url(r'^$', views.AllView.as_view(), name='all-hot-table'),
+    url(r'^restful/', include('HotDisplay.rest_urls')),
+#    url(r'^(?P<app>\w+)/(?P<model>\w+)$', views.TableView.as_view(), name='hot-table'),
     url(r'^disp/$', views.DisplayModel.as_view(), name='sk'),
     url(r'^disp/(?P<app>\w+)/(?P<model>\w+)$', views.DisplayModel.as_view(), name='sk'),
     url(r'^disp/(?P<app>\w+)/(?P<model>\w+)/(?P<id>\d+)$', views.DisplayItem.as_view(), name='sk'),
@@ -15,17 +17,9 @@ urlpatterns = patterns('SkeletalDisplay.views',
     url(r'^', include('password_reset.urls')),
 )
 
-urlpatterns += patterns('SkeletalDisplay.editor',
+urlpatterns += patterns('HotDisplay.editor',
     url(r'^add/(?P<app>\w+)/(?P<model>\w+)$', editor.AddEditItem.as_view(), name='add_item'),
     url(r'^edit/(?P<app>\w+)/(?P<model>\w+)/(?P<id>\d+)$', editor.AddEditItem.as_view(), name='edit_item'),
     url(r'^hot_edit/(?P<app>\w+)/(?P<model>\w+)$', editor.HotEdit.as_view(), name='hot_edit'),
     url(r'^delete/(?P<app>\w+)/(?P<model>\w+)/(?P<id>\d+)$', editor.DeleteItem.as_view(), name='delete_item'),
-=======
-import HotDjango.views as views
-
-urlpatterns = patterns('',
-#    url(r'^$', views.AllView.as_view(), name='all-hot-table'),
-    url(r'^restful/', include('HotDjango.rest_urls')),
-#    url(r'^(?P<app>\w+)/(?P<model>\w+)$', views.TableView.as_view(), name='hot-table'),
->>>>>>> handsontable-original
 )
